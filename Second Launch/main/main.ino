2af441b11d6e054b09457427a18a87495c25822a
Bensas <juanbensa@gmail.com>
2023-01-21 11:05:55 -0300

    Added launch, apogee and landing detection to main flight script

--- Second Launch/main/main.ino
+++ Second Launch/main/main.ino
@@ -1,9 +1,11 @@
 #include "Logger.h"
-#include "StorageModule.h"
-#include "CommunicationModule.h"
-#include "ElectromechanicalModule.h"
-#include "SensorModule.h"
+#include "MockStorageModule.h"
+#include "MockCommunicationModule.h"
+#include "MockElectromechanicalModule.h"
+#include "MockSensorModule.h"
 #include <stdint.h>
+
+#include <vector>
 
 // Enums
 #define STATE_STARTUP 0
@@ -11,9 +13,8 @@
 #define STATE_POST_APOGEE 2
 #define STATE_LANDED 3
 
-
 // Telemetry packet related data
-#define TELEMETRY_PACKET_STRING_LENGTH 34
+#define TELEMETRY_PACKET_STRING_LENGTH 36
 
 static const char startupStr[2] = {'S', 'T'};
 static const char preApogeeStr[10] = {'P', 'R'};
@@ -27,6 +28,16 @@ const char* STATE_STRING_ARRAY[4] = {startupStr, preApogeeStr, postApogeeStr, la
 uint8_t currentState = STATE_STARTUP;
 char telPacketString[TELEMETRY_PACKET_STRING_LENGTH]; // Assumed null terminated
 unsigned long lastMilis = 0;
+
+
+// Launch/Apogee detection algorithms
+#define MAX_LATEST_ALTITUDES 3
+#define MAX_LATEST_ALTITUDES_AFTER_APOGEE 7
+#define LAUNCH_DETECTION_MARGIN 5
+#define APOGEE_DETECTION_MARGIN 0.8
+#define LANDING_DETECTION_MARGIN 0.8
+#define LANDING_ALTITUDE 3
+std::vector<float> latestAltitudes;
 
 
 // Modules
@@ -56,8 +67,12 @@ void loop() {
       if (currMillis - lastMilis > 100) {
         float altitude = sensorModule.getAltitude();
         float acceleration = sensorModule.getAcceleration();
+
+        latestAltitudes.push_back(altitude);
+        if (latestAltitudes.size() > MAX_LATEST_ALTITUDES) latestAltitudes.erase(latestAltitudes.begin());
         
         if (detectLaunch(altitude, acceleration)){
+          Logger::log("Vehicle launched.");
           switchToState(STATE_PRE_APOGEE);
         }
         lastMilis = currMillis;
@@ -70,7 +85,10 @@ void loop() {
           double gpsLat = sensorModule.gps.location.lat(); // TODO: We could avoid sensing GPS before apogee
           double gpsLng = sensorModule.gps.location.lng();
 
-          createTelemetryPacketStr(communicationModule.packetCount, currentState, altitude, 0, 0);
+          latestAltitudes.push_back(altitude);
+          if (latestAltitudes.size() > MAX_LATEST_ALTITUDES) latestAltitudes.erase(latestAltitudes.begin());
+
+          createTelemetryPacketStr(communicationModule.packetCount, currentState, altitude, gpsLat, gpsLng); // TODO: Altitude should be float in telemetry packet?
           storageModule.addTelemetryPacketToFlightLog(telPacketString);
           communicationModule.sendTelemetryPacketToGround(telPacketString, TELEMETRY_PACKET_STRING_LENGTH);
 
@@ -89,13 +107,15 @@ void loop() {
             double gpsLat = sensorModule.gps.location.lat();
             double gpsLng = sensorModule.gps.location.lng();
 
-            createTelemetryPacketStr(communicationModule.packetCount, currentState, altitude, 0, 0);
+            latestAltitudes.push_back(altitude);
+            if (latestAltitudes.size() > MAX_LATEST_ALTITUDES_AFTER_APOGEE) latestAltitudes.erase(latestAltitudes.begin());
+
+            createTelemetryPacketStr(communicationModule.packetCount, currentState, altitude, gpsLat, gpsLng);
             storageModule.addTelemetryPacketToFlightLog(telPacketString);
             communicationModule.sendTelemetryPacketToGround(telPacketString, TELEMETRY_PACKET_STRING_LENGTH);
 
             if (detectLanding(altitude, acceleration)){
-              Serial.println("Landed. :)");
-              Serial.println(altitude);
+              Logger::log("Vehicle landed :)");
               switchToState(STATE_LANDED);
             }
             lastMilis = currMillis;
@@ -106,7 +126,7 @@ void loop() {
             double gpsLat = sensorModule.gps.location.lat();
             double gpsLng = sensorModule.gps.location.lng();
 
-            createTelemetryPacketStr(communicationModule.packetCount, currentState, 0, 0, 0);
+            createTelemetryPacketStr(communicationModule.packetCount, currentState, 0, gpsLat, gpsLng);
             communicationModule.sendTelemetryPacketToGround(telPacketString, TELEMETRY_PACKET_STRING_LENGTH);
 
             lastMilis = currMillis;
@@ -120,26 +140,52 @@ void switchToState(int8_t newState) {
   Logger::log("Switching to state: " + String(currentState));
   storageModule.saveCurrentState(currentState);
   if (currentState == STATE_POST_APOGEE){
-    Serial.println("Deploying parachute");
+    Logger::log("Deploying parachute");
     electromechanicalModule.deployParachute();
   }
 }
 
 bool detectLaunch(float currAltitude, float currAccel) {
-  Serial.println("Detecting launch...");
-  return true;
+  if (latestAltitudes.size() < MAX_LATEST_ALTITUDES) return false;
+  if (biggerThan(latestAltitudes.at(latestAltitudes.size()-1), latestAltitudes.at(latestAltitudes.size()-2), LAUNCH_DETECTION_MARGIN))
+      return true;
+  return false;
 }
 
 bool detectApogee(float currAltitude, float currAccel) {
+  if (latestAltitudes.size() < MAX_LATEST_ALTITUDES) return false;
+  if (smallerThan(latestAltitudes.at(2), latestAltitudes.at(1), APOGEE_DETECTION_MARGIN) &&
+      smallerThan(latestAltitudes.at(1), latestAltitudes.at(0), APOGEE_DETECTION_MARGIN))
+      return true;
   return false;
 }
 
 bool detectLanding(float currAltitude, float currAccel) {
-  return false;
+  Logger::debug("Detecting landing...");
+  return latestAltitudes.back() < LANDING_ALTITUDE;
+  // if (latestAltitudes.size() < MAX_LATEST_ALTITUDES_AFTER_APOGEE) return false;
+  // for (int i = 1; i < latestAltitudes.size(); i++) {
+  //   if (!equals(latestAltitudes.at(i), latestAltitudes.at(i-1), LANDING_DETECTION_MARGIN))
+  //     return false;
+  // }
+  // return true;
+}
+
+bool biggerThan(float v1, float v2, float margin) {
+  return v1 - v2 > margin;
+}
+
+bool smallerThan(float v1, float v2, float margin) {
+  return v2 - v1 > margin;
+}
+
+bool equals(float v1, float v2, float margin) {
+  return abs(v1 - v2) < margin;
 }
 
 // Format is <PACKET_COUNT>,<STATE>,<ALTITUDE>,<GPS_LATITUDE>,<GPS_LONGITUDE>
-// Example result: 10000,ST,9999,42.30402,34.30402
+// Example result: 10000,ST,9999.9, 42.30402, 34.30402
+//                 00021,PO,0604.2,-34.43555,-58.92193
 char* createTelemetryPacketStr(uint16_t packetCount, uint8_t currentState, float altitude, double gpsLat, double gpsLng) {
   Logger::debug("Creating telemetry packet...");
 
@@ -155,13 +201,23 @@ char* createTelemetryPacketStr(uint16_t packetCount, uint8_t currentState, float
   
   telPacketString[15] = ',';
   
-  sprintf(telPacketString + 16, "%08.5f", gpsLat);
+  if (gpsLat < 0){
+    sprintf(telPacketString + 16, "%08.5f", gpsLat);
+  } else {
+    telPacketString[16] = ' ';
+    sprintf(telPacketString + 17, "%08.5f", gpsLat);
+  }
   
-  telPacketString[24] = ',';
+  telPacketString[25] = ',';
 
-  sprintf(telPacketString + 25, "%08.5f", gpsLng);
+  if (gpsLng < 0) {
+    sprintf(telPacketString + 26, "%08.5f", gpsLng);
+  } else {
+    telPacketString[26] = ' ';
+    sprintf(telPacketString + 27, "%08.5f", gpsLng);    
+  }
   
-  telPacketString[33] = '\0';
+  telPacketString[35] = '\0';
   
   return telPacketString;
 }
